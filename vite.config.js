e92512b3ff236ca63c399e162e7b0b83275d3faf
Ilia <istudyatuni@gmail.com>
2025-06-19 23:21:04 +0300

    configure vite for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,11 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
 	plugins: [svelte()],
+	base: '/simple-timer',
+	build: {
+		sourcemap: true,
+		rollupOptions: {
+			treeshake: true,
+		},
+	},
 });
