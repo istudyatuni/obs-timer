002329b74d542b99c9cb274709767054aa3f8a25
Ilia <istudyatuni@gmail.com>
2025-06-19 23:58:14 +0300

    format

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
 	plugins: [svelte()],
-	base: '/simple-timer',
+	base: "/simple-timer",
 	build: {
 		sourcemap: true,
 		rollupOptions: {
