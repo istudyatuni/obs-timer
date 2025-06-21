2a4978721f849bbd143ee72a6a54c423634cb544
Ilia <istudyatuni@gmail.com>
2025-06-21 16:42:13 +0300

    refactor: move font settings to component

--- src/components/Settings.svelte
+++ src/components/Settings.svelte
@@ -2,12 +2,12 @@
   import GithubIcon from "~icons/tabler/brand-github";
 
   import SettingsCheckbox from "./SettingsCheckbox.svelte";
+  import SettingsFontOptions from "./SettingsFontOptions.svelte";
   import SettingsTimeInput from "./SettingsTimeInput.svelte";
   import SettingsTimerEntry from "./SettingsTimerEntry.svelte";
 
   import { CLOCK_POSITION_NAMES, CLOCK_POSITIONS } from "../lib/constants";
   import {
-    DEFAULT_STORAGE,
     HIDE_UI,
     PADDING_STORAGE_KEYS,
     SETTINGS_HIDDEN,
@@ -23,14 +23,6 @@
   function handle_reset_paddings() {
     for (let key of PADDING_STORAGE_KEYS) {
       STATE.set(key, 1);
-    }
-  }
-  function set_default_text(e) {
-    if (e.target.value.trim() == "") {
-      STATE.set(
-        "clockwatch_font_family",
-        DEFAULT_STORAGE.clockwatch_font_family,
-      );
     }
   }
 </script>
@@ -58,27 +50,7 @@
 <div class="settings-wrapper">
   <div class:hidden={$SETTINGS_HIDDEN || $HIDE_UI} class="settings">
     <SettingsTimeInput />
-    <div class="pad-top">
-      <label>
-        Font size:
-        <input
-          type="number"
-          bind:value={$STATE.clockwatch_font_size_em}
-          onwheel={() => {}}
-          min="1"
-          max="20"
-          step="0.1" />
-      </label>
-    </div>
-    <div class="pad-top">
-      <label>
-        Font family:
-        <input
-          type="text"
-          bind:value={$STATE.clockwatch_font_family}
-          onblur={set_default_text} />
-      </label>
-    </div>
+    <SettingsFontOptions />
     <SettingsCheckbox bind:checked={$STATE.clockwatch_tick_when_closed}
       >Tick when page is closed</SettingsCheckbox>
     <SettingsCheckbox bind:checked={$STATE.hide_empty_hour}
@@ -114,7 +86,7 @@
         default
       {/if}
     </p>
-    <details class="details" class:hidden={$STORED_TIMERS.length === 1}>
+    <details class:hidden={$STORED_TIMERS.length === 1}>
       <summary>Saved timers</summary>
       <ul>
         {#each $STORED_TIMERS.toSorted() as timer_name (timer_name)}
@@ -180,8 +152,5 @@
   }
   .footer {
     text-align: center;
-  }
-  input[type="text"] {
-    field-sizing: content;
   }
 </style>
---
+++ src/components/SettingsFontOptions.svelte
@@ -0,0 +1,42 @@
+<script module>
+  import { DEFAULT_STORAGE, STATE } from "../lib/stores";
+</script>
+
+<script>
+  function set_default_text(e) {
+    if (e.target.value.trim() == "") {
+      STATE.set(
+        "clockwatch_font_family",
+        DEFAULT_STORAGE.clockwatch_font_family,
+      );
+    }
+  }
+</script>
+
+<div class="pad-top">
+  <label>
+    Font size:
+    <input
+      type="number"
+      bind:value={$STATE.clockwatch_font_size_em}
+      onwheel={() => {}}
+      min="1"
+      max="20"
+      step="0.1" />
+  </label>
+</div>
+<div class="pad-top">
+  <label>
+    Font family:
+    <input
+      type="text"
+      bind:value={$STATE.clockwatch_font_family}
+      onblur={set_default_text} />
+  </label>
+</div>
+
+<style lang="scss">
+  input[type="text"] {
+    field-sizing: content;
+  }
+</style>
