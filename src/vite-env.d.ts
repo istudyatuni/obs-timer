fca2f5c898bfa7bd5b3c198789ff1f9c853c6a1b
Ilia <istudyatuni@gmail.com>
2025-06-21 10:02:56 +0300

    add icons

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,4 @@
-/// <reference types="svelte" />
 /// <reference types="obs-studio" />
+/// <reference types="svelte" />
+/// <reference types="unplugin-icons/types/svelte" />
 /// <reference types="vite/client" />
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import Icons from "unplugin-icons/vite";
 
 export default defineConfig({
-	plugins: [svelte()],
+	plugins: [svelte(), Icons({ compiler: "svelte" })],
 	base: "/simple-timer",
 	build: {
 		sourcemap: true,
